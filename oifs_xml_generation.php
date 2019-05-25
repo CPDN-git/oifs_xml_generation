b3d3fb2187598079de8cabe09ac05ea963683789
Sarah Sparrow <sarah.sparrow@oerc.ox.ac.uk>
2019-05-25 20:48:36 +0100

    Addition of webform to create xml file

---
+++ oifs_xml_generation.php
@@ -0,0 +1,150 @@
+<?php
+// This file is part of BOINC.
+// http://boinc.berkeley.edu
+// Copyright (C) 2008 University of California
+//
+// BOINC is free software; you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation,
+// either version 3 of the License, or (at your option) any later version.
+//
+// BOINC is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with BOINC.  If not, see <http://www.gnu.org/licenses/>.
+
+require_once("../inc/util.inc");
+require_once("../inc/user.inc");
+require_once("../inc/oifs_uploaders.inc");
+
+//page_head("OpenIFS Ancil upload form");
+
+echo <<<EOH
+<html>
+<head>
+<title>OpenIFS@home Ensemble Generation</title>
+<META NAME="ROBOTS" CONTENT="NOINDEV, NOFOLLOW">
+<script type="text/javascript" src="jquery/jquery-latest.js"></script>
+<script type="text/javascript" src="jquery/jquery.tablesorter.js"></script>
+<script type="text/javascript">
+                        $(document).ready(function() {
+                                $("#myTable").tablesorter();
+                        });
+                </script>
+<style type="text/css">
+                        #sortedtable thead th {
+                                color: #00f;
+                                font-weight: bold;
+                                text-decoration: underline;
+                        }
+                </style>
+<link rel="stylesheet" href="cpdn.css">
+</head>
+<body>
+EOH;
+
+echo '<div class="wrap" style="width:100%">';
+echo '<div style="width:100%">';
+echo '<img src="img/OIFS_Home_logo.png" alt="OpenIFS@home" style="width:200px">';
+echo '<img src="img/CPDN_logo_abbrv_sq.png" alt="CPDN" style="width:90px; float:right;">';
+echo '</div>';
+echo '<div style="clear: both;"></div>';
+echo '</div>';
+echo '<hr>';
+echo '<h2>To generate a submission xml enter the following information about your experiment file(s).</h2>';
+
+$user = get_logged_in_user();
+if (in_array($user->email_addr,$allowed_uploaders)){
+	echo "<p>$user->name is logged in</p>";
+	?>
+	<p><b>Use comma separated lists to enter mutliple values of fields (e.g. start date and batch owner) if required.</b></p>
+	<form id="oifs_xml_form" name="oifs_xml_form"  action="oifs_xml_handler.php" method="post" enctype="multipart/form-data">
+	
+	<div id="BatchInfo" name="BatchInfo">
+        <h3>Batch Information</h3>
+	<table width="100%" border="0" style="border:none;">
+	<tr class="nohover"><td Width=22%>Batch Project:</td><td width=78%><input type="text" name="BatchProj" value="OpenIFS@HOME"></td></tr>
+	<tr class="nohover"><td>Batch Name:</td><td><textarea id="BatchName" name="BatchName" rows="1" cols="80"></textarea></td></tr>
+	<tr class="nohover"><td>Batch Description:</td><td><textarea id="BatchDesc" name="BatchDesc" rows="3" cols="80"></textarea></td></tr>
+	<tr class="nohover"><td>Batch Owner(s) as Name (e-mail):</td><td><textarea id="BatchOwner" name="BatchOwner" rows="1" cols="80"></textarea></td></tr>
+	<tr class="nohover"><td>Batch Technical Information:</td><td><textarea id="BatchTechInfo" name="BatchTechInfo" rows="3" cols="80"></textarea></td></tr>
+	</table>
+	</div>
+	
+	<div id="config" name="config">
+	<h3>Model Configuration Details</h3>
+	<table width="100%" border="0" style="border:none;">
+	<tr class="nohover"><td width=22%>Model Class:</td><td width=78%><input type="text" name="model_class" value="openifs"></td></tr>
+	<tr class="nohover"><td>Model Configuration File:</td><td><input type="text" name="model_config" value="40r1_T159.xml"></td></tr>
+	</table>
+	</div>
+
+	<div id="setup" name="setup">
+        <h3>Ensemble Setup</h3>
+	<table width="100%" border="0" style="border:none;">
+	<tr class="nohover"><td width=22%>Start date(s) as YYYYMMDDHH:</td><td width=78%><textarea id="start_dates" name="start_dates" rows="1" cols="80"></textarea></td></tr>
+	<tr class="nohover"><td>Starting UMID:</td><td><input type="text" name="start_umid" value="a000"></td></tr>
+	<tr class="nohover"><td>Number of analyses (per start date):</td><td><input type="text" name="n_analysis"></td></tr>
+	<tr class="nohover"><td>Number of ensemble members (per analysis):</td><td><input type="text" name="n_ens"></td></tr>
+	<tr class="nohover"><td>Upload Location:</td><td><select id="upload_loc" name="upload_loc" class="dropdown">
+                <option value="0">Select...</option>
+                <option value="alpha">Alpha</option>
+                <option value="dev">Dev</option>
+                <option value="upload2">Upload2</option>
+                <option value="upload3">Upload3</option>
+                <option value="upload4">Upload4</option>
+                <option value="upload5">Upload5</option>
+                <option value="upload6">Upload6</option>
+                <option value="upload7">Upload7</option>
+                <option value="upload8">Upload8</option>
+                <option value="upload9">Upload9</option>
+                <option value="upload10">Upload10</option>
+                <option value="upload11">Upload11</option>
+                <option value="upload12">Upload12</option>
+                </select></td></tr>
+	</table>
+        </div>
+
+	<div id="ens_config" name="ens_config">
+        <h3>Ensemble Configuration</h3>
+	<table width="100%" border="0" style="border:none;">
+	<tr class="nohover"><td Width=22%>ECMWF Experiment ID:</td><td width=78%><input type="text" name="exptid"></td></tr>
+        <tr class="nohover"><td>Forecast Length:</td><td><input type="text" name="fclen">
+	<select id="fclen_units" name="fclen_units" class="dropdown">
+                <option value="days">Days</option>
+                <option value="hours">Hours</option>
+                </select></td></tr>
+        <tr class="nohover"><td>FullPos Namelist File:</td><td><input type="text" name="fullpos_namelist"></td></tr>
+	</table>
+        </div>
+
+	<div id="ifsdata" name="ifsdata">
+	<h3>IFS data</h3>
+	<table width="100%" border="0" style="border:none;">
+        <tr class="nohover"><td Width=22%>SO4 File:</td><td width=78%><input type="text" name="SO4_file" value="SO4.zip"></td></tr>
+        <tr class="nohover"><td>Radiation File:</td><td width=78%><input type="text" name="Rad_file" value="radiation.zip"></td></tr>
+        <tr class="nohover"><td>CFC File:</td><td width=78%><input type="text" name="CFC_file" value="CFC.zip"></td></tr>
+	</table>
+        </div>
+
+	<div id="climate_data" name="climate_data">
+        <h3>Climate Data</h3>
+        <table width="100%" border="0" style="border:none;">
+	<tr class="nohover"><td Width=22%>Climate Data File:</td><td width=78%><input type="text" name="climate_data_file" value="climate_data.zip"></td></tr>
+        </table>
+	</div>
+	<br>
+	<br>
+	<input class="button" type="submit" name="submit" value="Create" />	
+<form>
+
+<?php 
+}
+else {
+	echo "You are not allowed to visit this page";
+	}
+
+?>
---
+++ oifs_xml_handler.php
@@ -0,0 +1,78 @@
+<?php
+// This file is part of BOINC.
+// http://boinc.berkeley.edu
+// Copyright (C) 2008 University of California
+//
+// BOINC is free software; you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation,
+// either version 3 of the License, or (at your option) any later version.
+//
+// BOINC is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with BOINC.  If not, see <http://www.gnu.org/licenses/>.
+
+require_once("../inc/util.inc");
+require_once("../inc/user.inc");
+require_once("../inc/boinc_db.inc");
+require_once("../inc/oifs_uploaders.inc");
+
+
+//page_head("OpenIFS Ensemble Creation");
+
+echo <<<EOH
+<html>
+<head>
+<title>OpenIFS@home Ancil upload form</title>
+<script type="text/javascript" src="jquery/jquery-latest.js"></script>
+<script type="text/javascript" src="jquery/jquery.tablesorter.js"></script>
+<script type="text/javascript">
+                        $(document).ready(function() {
+                                $("#myTable").tablesorter();
+                        });
+                </script>
+<style type="text/css">
+                        #sortedtable thead th {
+                                color: #00f;
+                                font-weight: bold;
+                                text-decoration: underline;
+                        }
+                </style>
+<link rel="stylesheet" href="cpdn.css">
+</head>
+<body>
+EOH;
+
+echo '<div class="wrap" style="width:100%">';
+echo '<div style="width:100%">';
+echo '<img src="img/OIFS_Home_logo.png" alt="OpenIFS@home" style="width:200px">';
+echo '<img src="img/CPDN_logo_abbrv_sq.png" alt="CPDN" style="width:90px; float:right;">';
+echo '</div>';
+echo '<div style="clear: both;"></div>';
+echo '</div>';
+echo '<hr>';
+
+$user = get_logged_in_user();
+
+$python_env='/home/boinc/miniconda2/envs/oifs_pyenv/bin/python';
+$script_path='/storage/www/cpdnboinc_dev/oifs_xml_generation/';
+
+if (in_array($user->email_addr,$allowed_uploaders)){
+        echo "$user->name is logged in<br>";
+        echo 'Form was submitted, here are the form values: <pre>';
+        print_r($_POST);
+        echo "</pre>";
+	$arr= json_encode($_POST);
+	$escaped_arr=escapeshellarg($arr);
+	$r = escapeshellcmd( $python_env.' '.$script_path.'create_basic_xml_webform.py '.$escaped_arr);
+	$output = shell_exec($r);
+	echo "<pre>$output</pre>";
+}
+else {
+        die("You are not allowed to visit this page");
+        } ?>
+
